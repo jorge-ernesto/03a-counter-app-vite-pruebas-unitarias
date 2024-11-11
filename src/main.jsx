9449ff0413c42b64e6fa98dbe36a705606635da2
Ernesto <jorge.cywdt@gmail.com>
2024-11-11 16:48:05 -0500

    Cambios realizados por Victor Robles
    ---- El proyecto requiere ----
    - Node 18.14.0
    
    ---- Acciones ----
    - Eliminamos "node_modules" y "package-lock.json", para ejecutar "npm install"
    - Si existiera "node_modules" y "package-lock.json", ejecutar "npm update"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+// import './assets/css/index.css';
+// import App from './App';
+
+import './assets/css/MaquetacionCurso/styles.css';
+// import { HelloWorldApp } from './components/HelloWorldApp';
+// import { FirstApp } from './components/FirstApp';
+import { CounterApp } from './components/CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        {/* <App /> */}
+        {/* <HellowRowldApp /> */}
+        {/* <FirstApp title="Hola, soy Goku" subTitle={123} /> */}
+        <CounterApp value={10} />
+    </React.StrictMode>,
 )
